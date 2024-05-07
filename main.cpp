3bd4574949129ed22d4d1c0c4757aa37267d3570
samuel <samiretru@gmail.com>
2024-05-07 15:35:46 -0500

    mejora de funcionalidad de editar

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 
     if (argc != 3) {
         cerr << "Uso: " << argv[0] << " <nombre de archivo sin extension> " << "<opcion>";
-    }
+    }   
 
     string nombre_archivo = argv[1];
     string opcion = argv[2];
@@ -40,21 +40,29 @@ int main(int argc, char *argv[]) {
 
         vector<int> comprimido = comprimir_lzw(entrada); 
 
-        for (int num: comprimido) {
-            cout << num << " ";
-
-        }
-
-        cout << endl;
-
         escribir_archivo_comprimido(comprimido, nombre_archivo);
+
+        cout << "Archivo comprimido y escrito exitosamente." << endl;
 
     }
 
     if (opcion == "editar") {
+        cout << "Contenido del archivo comprimido: " << endl;
+
         vector<int> contenido = leer_archivo_comprimido(nombre_archivo);
         string descomprimido = descomprimir_lzw(contenido);
         cout << descomprimido;
+
+        string entrada = leer_entrada();
+
+        entrada = descomprimido + entrada;
+
+        vector<int> comprimido = comprimir_lzw(entrada); 
+
+        escribir_archivo_comprimido(comprimido, nombre_archivo);
+        escribir_archivo_plano(entrada, nombre_archivo);
+
+        cout << "Archivo editado exitosamente" << endl;
     }
 
     return 0;
